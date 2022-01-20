ef71428bf6a0a59843e7ae39fa04f53407bea37e
Sharvari Govele <91045770+Sharvari-Govele@users.noreply.github.com>
2022-01-20 20:37:40 +0530

    Add files via upload
    Linear search algorithm that searches for multiple occurrences of value with their locations.

---
+++ linear search/linear search2.py
@@ -0,0 +1,13 @@
+a=[]
+n=int(input('enter number of elements'))
+for i in range (0,n):
+    x=int(input("Enter number"))
+    a.append(x)
+print(a)
+val=int(input("Enter value to be searched"))
+for j in range (0,len(a)):
+ if a[j] == val:
+  print(val,'found at location',j)
+ else :
+  print('not found')
+  break
