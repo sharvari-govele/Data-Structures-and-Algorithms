10def84093a7f7b46eaf208b1e6e2cfbf9dcb096
Sharvari Govele <91045770+Sharvari-Govele@users.noreply.github.com>
2022-01-21 15:05:58 +0530

    Add files via upload
    binary search on ascending sorted array

---
+++ Binary search/Binary search ascending sort.py
@@ -0,0 +1,20 @@
+a=[]
+n=int(input('enter number of elements'))
+for i in range (0,n):
+    x=int(input("Enter number"))
+    a.append(x)
+print(a)
+val=int(input("Enter value to be searched"))
+l=0
+r=n-1
+while(l<=r):
+    m=int((l+r)/2)
+    if val>a[m]:
+      l=m+1
+    elif val<a[m]:
+      r=m-1
+    else :
+      print('location is',m+1) 
+      break 
+if l>r:
+    print('no such value found')
